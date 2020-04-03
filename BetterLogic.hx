27d94cc0f5652aeaea2d8aa54c4de1a143b523a1
Evan Svendsen <evan.svendsen@uleth.ca>
2020-04-02 19:38:05 -0600

    Added inplace add and sub blocks. Removed break. Condensed logic to xml file

--- BetterLogic.hx
+++ BetterLogic.hx
@@ -1,34 +1,3 @@
 class BetterLogic {
-
-	public static var Switch:Null<Dynamic> = null;
-	public static var FoundCase:Bool;
-
-	/***** Sets the switch value *****/
-	public static function SetSwitch(_s:Dynamic)
-	{
-		FoundCase = false;
-		Switch = _s;
-	}
-
-	/***** Exits Switch Statement *****/
-	public static function ClearSwitch()
-	{
-		FoundCase = true;
-		Switch = null;
-	}
-
-	/***** Subtract *****/
-	public static function SubReturn(_num:Int) : Int
-	{
-		_num = _num - 1;
-		return _num;
-	}
-
-	/***** Add *****/
-	public static function AddReturn(_num:Int) : Int
-	{
-		_num = _num + 1;
-		return _num;
-	}
 	
 }
